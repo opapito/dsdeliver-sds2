6cb89dcd4189b81c46d4f2ec374b671d7f199001
opapito <51033778+opapito@users.noreply.github.com>
2021-02-16 16:26:20 -0800

    Add GitHub icon

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -2,20 +2,24 @@ import './styles.css';
 import { ReactComponent as YouTubeIcon } from './youtube.svg';
 import { ReactComponent as LinkedinIcon } from './linkedin.svg';
 import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as GitHubIcon } from './github.svg';
 
 function Footer(){
   return(
     <footer className="main-footer">
       App developed during the 2º edition of DevSuperior week
       <div className="footer-icons">
-        <a href="https://www.youtube.com/c//DevSuperior" target="_new">
+        <a href="https://www.youtube.com" target="_new">
           <YouTubeIcon />
         </a>
-        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+        <a href="https://www.linkedin.com" target="_new">
           <LinkedinIcon />
         </a>
-        <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+        <a href="https://www.instagram.com" target="_new">
           <InstagramIcon />
+        </a>
+        <a href="https://github.com/opapito/dsdeliver-sds2" target="_new">
+          <GitHubIcon />
         </a>
       </div>
     </footer>
